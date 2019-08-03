3d2c40c822ef5fe99732898857f5b015427b20a2
Mariela Torres <martz_maru@hotmail.com>
2019-08-03 15:03:30 -0500

    Agregando carpeta

---
+++ src/clientes.js
@@ -0,0 +1,113 @@
+var CLIENTES = {
+    "clientes": [{
+      "id": 1,
+      "intereses": [
+        "Ropa",
+        "Calzado",
+        "Servicios"
+      ],
+      "ofrece": [
+        "Servicios",
+        "Moda y belleza",
+        "Ropa"
+      ]
+    }, {
+        "id": 2,
+        "intereses": [
+          "Vehiculos",
+          "Calzado",
+          "Electronica",
+          "Moda y belleza"
+        ],
+        "ofrece": [
+          "Servicios",
+          "Ropa"
+        ]
+    },{
+        "id": 3,
+      "intereses": [
+        "Comestibles",
+        "Calzado"
+      ],
+      "ofrece": [
+        "Servicios",
+      ]
+    },{
+        "id": 4,
+      "intereses": [
+        "Comestibles",
+        "Calzado",
+        "Moda y belleza"
+      ],
+      "ofrece": [
+        "Servicios",
+      ]
+    },{
+        "id": 5,
+      "intereses": [
+        "Moda y belleza",
+        "Ropa"
+      ],
+      "ofrece": [
+        "Comestibles",
+        "Servicios"
+      ]
+    },{
+        "id": 6,
+      "intereses": [
+        "Servicios",
+        "Comestibles"
+      ],
+      "ofrece": [
+        "Ropa",
+        "Electronica"
+      ]
+    },{
+        "id": 7,
+      "intereses": [
+        "Vehiculos",
+        "Electronica"
+      ],
+      "ofrece": [
+        "Servicios",
+        "Comestibles"
+      ]
+    },{
+        "id": 8,
+      "intereses": [
+        "Ropa",
+        "Calzado"
+      ],
+      "ofrece": [
+        "Servicios",
+        "Ropa"
+      ]
+    },{
+        "id": 9,
+      "intereses": [
+        "Comestibles",
+        "Calzado",
+        "Ropa",
+        "Servicios"
+      ],
+      "ofrece": [
+        "Servicios",
+        "Comestibles",
+        "Calzado",
+        "Ropa",
+        "Vehiculos"
+      ]
+    },{
+        "id": 10,
+      "intereses": [
+        "Comestibles",
+        "Servicios",
+        "Electronica"
+      ],
+      "ofrece": [
+        "Servicios",
+        "Ropa",
+        "Electronica",
+        "Vehiculos"
+      ]
+    }
